1b3d6faca03841b1cee54ed2e19128c3896d8bb8
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-02-03 12:44:21 +0100

    Ejercicio 3

---
+++ Tema4/src/parte1/ejercicio3/PrincipalRectangulo.java
@@ -0,0 +1,29 @@
+package parte1.ejercicio3;
+
+public class PrincipalRectangulo {
+
+	public static void main(String[] args) {
+
+		// Creamos dos objetos Rectangulos para almacenar dos rectangulos distintos.
+		Rectangulo rect1 = new Rectangulo();
+		Rectangulo rect2 = new Rectangulo();
+		
+		//Creamos la variable perimetro para almacenar el perimero de cada rectangulo.
+		int perimetro;
+		
+		
+
+		// Guardamos las respectivas coordenadas del primer rectangulo.
+		rect1.x1 = 0;
+		rect1.y1 = 0;
+		rect1.x2 = 5;
+		rect1.y2 = 5;
+
+		// Guardamos las respectivas coordenadas del segundo rectangulo.
+		rect2.x1 = 0;
+		rect2.y1 = 0;
+		rect2.x2 = 5;
+		rect2.y2 = 5;
+	}
+
+}
---
+++ Tema4/src/parte1/ejercicio3/Rectangulo.java
@@ -0,0 +1,30 @@
+package parte1.ejercicio3;
+
+/**
+ * La clase Rectangulo se encarga de almacenar los atributos de los que consta
+ * un rectangulo.
+ */
+public class Rectangulo {
+
+	/**
+	 * Creamos el atributo x1 para almacenar la primera cordenada del rectangulo.
+	 */
+	int x1;
+
+	/**
+	 * Creamos el atributo y1 para almacenar la segunda cordenada del rectangulo.
+	 */
+	int y1;
+
+	/**
+	 * Creamos el atributo x2 para almacenar la primera cordenada de otro plano del
+	 * rectangulo.
+	 */
+	int x2;
+
+	/**
+	 * Creamos el atributo y2 para almacenar la primera cordenada de otro plano del
+	 * rectangulo.
+	 */
+	int y2;
+}
