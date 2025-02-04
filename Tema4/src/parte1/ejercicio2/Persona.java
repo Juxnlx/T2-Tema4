82a7445ceb3ddfe597026c74b72abac3d0d43a87
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-02-04 14:29:28 +0100

    Ejercicio 2 - Completo

--- Tema4/src/parte1/ejercicio2/Persona.java
+++ Tema4/src/parte1/ejercicio2/Persona.java
@@ -1,5 +1,9 @@
 package parte1.ejercicio2;
 
+/**
+ * Creamos la clase Persona para alamcenar los atributos de los que cosnta una
+ * persona.
+ */
 public class Persona {
 
 	// Creamos la varible dni como String para almacenar el dni de la persona.
@@ -15,4 +19,35 @@ public class Persona {
 
 	// Creamos la variable edad como int para almacenar la edad de la persona.
 	int edad;
+
+	/**
+	 * Creamos un constructor con parametros.
+	 * 
+	 * @param dni      DNI de la persona.
+	 * @param nombre   Nombre de la persona.
+	 * @param apellido Apellido de la persona.
+	 * @param edad     Edad de la persona.
+	 */
+	public Persona(String dni, String nombre, String apellido, int edad) {
+		//Comprobamos si el dni no esta a null ni vacio.
+		if (dni != null && !dni.isEmpty()) {
+			this.dni = dni;
+		}
+		
+		//Comprobamos si el nombre no esta a null ni vacio.
+		if (nombre != null && !nombre.isEmpty()) {
+			this.nombre = nombre;
+		}
+		
+		//Comprobamos si el apellido no esta a null ni vacio.
+		if (apellido != null && !apellido.isEmpty()) {
+			this.apellido = apellido;
+		}
+		
+		//Comprobamos si la edad es mayor que 0.
+		if (edad > 0) {
+			this.edad = edad;
+		}
+	}
+
 }
--- Tema4/src/parte1/ejercicio2/PrincipalPersona.java
+++ Tema4/src/parte1/ejercicio2/PrincipalPersona.java
@@ -2,32 +2,55 @@ package parte1.ejercicio2;
 
 import java.util.Scanner;
 
+/**
+ * Creamos dos objetos personas y pedimos la información al usuario por teclado
+ * para rellenarlos.
+ */
 public class PrincipalPersona {
 
 	public static void main(String[] args) {
 
 		// Creamos dos personas.
-		Persona pers1 = new Persona();
-		Persona pers2 = new Persona();
+		Persona pers1;
+		Persona pers2;
+
+		// Creamos la varible dni como String para almacenar el dni de la persona.
+		String dni;
+
+		// Creamos la variable nombre como String para almacenar el nombre de la
+		// persona.
+		String nombre;
+
+		// Creamos la variable apellidos como String para almacenar el apellido de la
+		// persona.
+		String apellido;
+
+		// Creamos la variable edad como int para almacenar la edad de la persona.
+		int edad;
 
 		// Creamos el Scanner para leer cada dato de las dos personas.
 		Scanner sc = new Scanner(System.in);
 
 		// Le pedimos al usuario que introduzca el dni de la persona 1 y lo leemos.
 		System.out.print("Introduce el dni de la 1º persona: ");
-		pers1.dni = sc.nextLine();
+		dni = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca el nombre de la persona 1 y lo leemos.
 		System.out.print("Introduce el nombre de la 1º persona: ");
-		pers1.nombre = sc.nextLine();
+		nombre = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca el apellido de la persona 1 y lo leemos.
 		System.out.print("Introduce el apellido de la 1º persona: ");
-		pers1.apellido = sc.nextLine();
+		apellido = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca la edad de la persona 1 y lo leemos.
 		System.out.print("Introduce la edad de la 1º persona: ");
-		pers1.edad = sc.nextInt();
+		edad = sc.nextInt();
+		// Limpiamos el buffer.
+		sc.nextLine();
+		
+		// Creamos la pers1 con los datos solicitados al usuario.
+		pers1 = new Persona(dni, nombre, apellido, edad);
 
 		/*
 		 * DATOS DE LA 2º PERSONA.
@@ -35,22 +58,25 @@ public class PrincipalPersona {
 
 		// Le pedimos al usuario que introduzca el dni de la persona 2 y lo leemos.
 		System.out.print("Introduce el dni de la 2º persona: ");
-		pers2.dni = sc.nextLine();
+		dni = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca el nombre de la persona 2 y lo leemos.
 		System.out.print("Introduce el nombre de la 2º persona: ");
-		pers2.nombre = sc.nextLine();
+		nombre = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca el apellido de la persona 2 y lo leemos.
 		System.out.print("Introduce el apellido de la 2º persona: ");
-		pers2.apellido = sc.nextLine();
+		apellido = sc.nextLine();
 
 		// Le pedimos al usuario que introduzca la edad de la persona 2 y lo leemos.
 		System.out.print("Introduce la edad de la 2º persona: ");
-		pers2.edad = sc.nextInt();
+		edad = sc.nextInt();
+
+		// Creamos la pers2 con los datos solicitados al usuario.
+		pers2 = new Persona(dni, nombre, apellido, edad);
 
 		System.out.println("\nPersonas introducidas por el usuario");
-		
+
 		// Imprimimos la primera persona
 		System.out.println("Persona 1: " + pers1.nombre + " " + pers1.apellido + " con dni " + pers1.dni
 				+ (pers1.edad < 18 ? " no es" : " es") + " mayor de edad.");
