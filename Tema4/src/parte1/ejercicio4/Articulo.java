15695b2503122a40c9294a13d3e88399a4c27a1b
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-02-09 13:29:57 +0100

    Ejercicio 4 - Completo

--- Tema4/src/parte1/ejercicio4/Articulo.java
+++ Tema4/src/parte1/ejercicio4/Articulo.java
@@ -122,14 +122,75 @@ public class Articulo {
 		return precioFinal;
 	}
 
+	/**
+	 * Esta función se encarga de calcular el precio del articulo con el precio ya
+	 * calculado.
+	 * 
+	 * @param descuento El porcentaje de descuento a aplicar al producto.
+	 * @return precioDescuento. El precio con el descuento ya aplicado.
+	 */
 	public double getPVPDescuento(int descuento) {
-		// Creamos la variable precioDescuento como double para almacenar el precio con
-		// el descuento ya aplicado.
-		double precioDescuento;
-		
-		precioDescuento = (this.precio - ((descuento * this.precio) / 100));
-		
-		//Devolvemos el precio con el descuento aplicado.
-		return precioDescuento;
+		// Calculamos el precio con el descuento ya aplicado.
+		this.precio = (this.precio - ((descuento * this.precio) / 100));
+
+		// Devolvemos el precio con el descuento aplicado.
+		return precio;
 	}
+
+	/**
+	 * Esta función se encarga de calcular si es posible o no efectuar una compra de
+	 * un producto, teniendo en cuenta la cantidad de articulos que quedan
+	 * disponibles.
+	 * 
+	 * @param cantidad. La cantidad de productos que se quieren vender.
+	 * @return ventaPosible. True si quedan articulos suficientes para vender, false
+	 *         si no.
+	 */
+	public boolean vender(int cantidad) {
+		// Creamos la variable ventaPosible como boolean para almacenar true o false si
+		// se puede o no, dependiendo a la cantidad del producto.
+		boolean ventaPosible = false;
+
+		// Comprobamos si el stock del articulo es mayor o igual que 0, estonces
+		// devolvemos true, porque entonces si se puede vender esa cantidad del
+		// articulo.
+		if ((this.cuantosQuedan - cantidad) >= 0) {
+			ventaPosible = true;
+			// Altualizamos la variable cuentosQuedan.
+			this.cuantosQuedan -= cantidad;
+		}
+
+		// Devolvemos si se ha podido o no efectuar la comprar.
+		return ventaPosible;
+	}
+
+	/**
+	 * Esta función se encarga de actualizar el stock del articulo.
+	 * 
+	 * @param cantidad. La cantidad de articulos nuevos que hemos repuesto.
+	 */
+	public void almacenar(int cantidad) {
+		// Añadimos al stock del articulo la cantidad de articulos nuevos a reponer.
+		this.cuantosQuedan += cantidad;
+	}
+
+	/**
+	 * Esta función se encarga de imprimir todos los datos de un articulo.
+	 * 
+	 * @return Una cadena donde se almacenan todos los datos de un articulo.
+	 */
+	public String toString() {
+		// Creamos la variable datosArticulo como String para almacenar todos los datos
+		// del articulo.
+		String datosArticulo;
+
+		datosArticulo = "Nombre: " + this.nombre + "\n";
+		datosArticulo += "Precio: " + getPVP() + "\n";
+		datosArticulo += "Stock: " + this.cuantosQuedan + "\n";
+
+		// Devolvemos la cadena datosArticulo, donde se almacena todos los datos de un
+		// articulo.
+		return datosArticulo;
+	}
+
 }
--- Tema4/src/parte1/ejercicio4/PrincipalArticulo.java
+++ Tema4/src/parte1/ejercicio4/PrincipalArticulo.java
@@ -1,5 +1,7 @@
 package parte1.ejercicio4;
 
+import java.util.Scanner;
+
 /**
  * Esta clase crea dos objetos Articulo con distintos datos cada uno e
  * imprimimos su precio final y luego lo volvemos a mostrar pero con el precio
@@ -12,32 +14,53 @@ public class PrincipalArticulo {
 		// Creamos un articulo.
 		Articulo art1 = new Articulo("Cascos", 86, 4);
 		Articulo art2 = new Articulo("Sudadera", 25, -1);
+		Articulo art3 = new Articulo("Zapatillas", 72, 10);
+
+		// Creamos la variable cantidad para almacenar la cantidad que decontar o sumar
+		// al stock del articulo.
+		int cantidad;
+
+		// Creamos el Scanner para solicitar la cantidad de articulos que quiere el
+		// usuario.
+		Scanner sc = new Scanner(System.in);
 
 		// Imprimimos la información el articulo.
 		System.out.println("1º Articulo");
-		System.out.println(art1.getNombre() + " - Precio:" + art1.getPrecio() + " - IVA:" + Articulo.IVA + "% - PVP:"
-				+ (((Articulo.IVA * art1.getPrecio()) / 100) + art1.getPrecio()));
+		System.out.println(art1);
 
 		// Modificamos el precio del articulo.
-		art1.setPrecio(art1.getPrecio()-12);
+		art1.setPrecio(art1.getPrecio() - 12);
 
 		// Volvemos a imprimir el articulo
 		System.out.println("Articulo con precio modificado -->");
-		System.out.println(art1.getNombre() + " - Precio:" + art1.getPrecio() + " - IVA:" + Articulo.IVA + "% - PVP:"
-				+ (((Articulo.IVA * art1.getPrecio()) / 100) + art1.getPrecio()));
+		System.out.println(art1);
 
 		// Imprimimos la información el articulo.
 		System.out.println("\n2º Articulo");
-		System.out.println(art2.getNombre() + " - Precio:" + art2.getPrecio() + " - IVA:" + Articulo.IVA + "% - PVP:"
-				+ (((Articulo.IVA * art2.getPrecio()) / 100) + art2.getPrecio()));
+		System.out.println(art2);
 
 		// Modificamos el precio del articulo.
-		art2.setPrecio(art2.getPrecio()-10);
+		art2.setPrecio(art2.getPrecio() - 10);
 
 		// Volvemos a imprimir el articulo
 		System.out.println("Articulo con precio modificado -->");
-		System.out.println(art2.getNombre() + " - Precio:" + art2.getPrecio() + " - IVA:" + Articulo.IVA + "% - PVP:"
-				+ (((Articulo.IVA * art2.getPrecio()) / 100) + art2.getPrecio()));
+		System.out.println(art2);
+
+		System.out.println("\n3º Articulo");
+		System.out.println("Le pedimos al usuario que introduzca la cantidad de zapatillas que quiere: ");
+		cantidad = sc.nextInt();
+
+		if (art3.vender(cantidad)) {
+			System.out.println("Enhorabuena la venta ha sido posible.");
+		} else {
+			System.out.println("Lo siento, no hay suficientes productos es stock.");
+		}
+
+		System.out.println(art3);
+
+		// Cierre de Scanner
+		sc.close();
+		
 	}
 
 }
