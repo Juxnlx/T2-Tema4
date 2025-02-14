827d3c98764575bde5358bf79f4cf23c430b1035
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-02-14 14:40:38 +0100

    Ejercicio 1 Part3 - En proceso...

---
+++ Tema4/src/parte3/ejercicio1/Suma.java
@@ -0,0 +1,44 @@
+package parte3.ejercicio1;
+
+public class Suma {
+
+	/**
+	 * Esta función recibe dos números y se encarga de calcular la suma de dichos
+	 * números.
+	 * 
+	 * @param num1 El primer número entero pasado como parametro.
+	 * @param num2 El segundo número entero pasado como parametro.
+	 * @return La suma de los dos números.
+	 */
+	public int suma(int num1, int num2) {
+		// Creamos la variable sumaNumeros como int para almmacenar el valor de las dos
+		// sumas pasadas como parametro.
+		int sumaNumeros;
+
+		// Realizamos la ecuación y lo alamcenamos en sumaNumeros.
+		sumaNumeros = num1 + num2;
+
+		// Devolvemos la variable sumaNumeros donde se almacena el resultado de la suma
+		return sumaNumeros;
+	}
+
+	/**
+	 * Esta función recibe dos números y se encarga de calcular la suma de dichos
+	 * números.
+	 * 
+	 * @param num1 El primer número pasado como parametro.
+	 * @param num2 El segundo número pasado como parametro.
+	 * @return La suma de los dos números.F
+	 */
+	public int suma(int num1, int num2) {
+		// Creamos la variable sumaNumeros como int para almmacenar el valor de las dos
+		// sumas pasadas como parametro.
+		int sumaNumeros;
+
+		// Realizamos la ecuación y lo alamcenamos en sumaNumeros.
+		sumaNumeros = num1 + num2;
+
+		// Devolvemos la variable sumaNumeros donde se almacena el resultado de la suma
+		return sumaNumeros;
+	}
+}
