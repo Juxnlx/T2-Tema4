b341f2c6ccbc60af6899b490594f6795c5eaec38
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-02-17 12:42:55 +0100

    Ejercicio 6 - Casi Completo

--- Tema4/src/parte3/ejercicio6/FuncionesMatematicas.java
+++ Tema4/src/parte3/ejercicio6/FuncionesMatematicas.java
@@ -18,27 +18,70 @@ public class FuncionesMatematicas {
 		if (numero == 1) {
 			sol = 1;
 
-			// CASO RECURSIVO - La ecuación seria el número menos la llamada a la propia
+			// CASO RECURSIVO - La ecuación seria el número mas la llamada a la propia
 			// función como parametro numero -1.
 		} else {
-			sol = numero - sumatorio(numero - 1);
+			sol = numero + sumatorio(numero - 1);
 		}
 
 		// Devolvemos la variable sol, donde se almacena la suma de todos los números.
 		return sol;
 	}
 
+	/**
+	 * Esta función se encarga de calcular la potencia de un número.
+	 * 
+	 * @param a El número que sera elevado.
+	 * @param n El valor por el que se elevará el número.
+	 * @return El número ya elevado.
+	 */
 	public static double potencia(double a, int n) {
 		// Creamos la variable sol como double para almacenar la potencia de a.
 		double sol;
-		
+
+		// CASO BASE - Comprobamos si el elevado es 0 porque en ese caso la solución es
+		// igual a 1.
 		if (n == 0) {
 			sol = 1;
+			// CASO RECURSIVO - Para calcular la potencia multiplicamos el número por el
+			// número elevado a un exponente menor, para ello llamamos a la función de
+			// nuevo.
 		} else {
-			sol = a * Math.pow(a, n-1);
+			sol = a * Math.pow(a, n - 1);
 		}
-		
-		//Devolvemos la variable a, donde se almacena la potencia de a.
+
+		// Devolvemos la variable a, donde se almacena la potencia de a.
 		return sol;
+	}
+
+	/**
+	 * Esta función calcula cual es el valor de fibonacci segun la posición pasada
+	 * como parametro.
+	 * 
+	 * @param numero La posición del valor que queremos saber en fibonacci.
+	 * @return El valor que corresponda con la posición pasada como parametro.
+	 */
+	public static int serieFibonacci(int numero) {
+
+		// Creamos la variable fibonacci como int para almacenar el valor de la posición
+		// pasada por parametro.
+		int fibonacci;
+
+		// CASE BASE - Comprobamos si el número es igual a 1, en ese caso el resultado
+		// es igual a 1.
+		if (numero == 1) {
+			fibonacci = 1;
+			// CASE BASE - Comprobamos si el número es igual a 2, en ese caso el resultado
+			// es igual a 1.
+		} else if (numero == 2) {
+			fibonacci = 1;
+			//CASO RECURSIVO - 
+		} else {
+			fibonacci = serieFibonacci(numero - 1) + serieFibonacci(numero - 2);
+		}
+
+		// Devolvemos la variable fibonacci, donde se almacena el número de dicha
+		// posición.
+		return fibonacci;
 	}
 }
---
+++ Tema4/src/parte3/ejercicio6/PrincipalFMate.java
@@ -0,0 +1,14 @@
+package parte3.ejercicio6;
+
+public class PrincipalFMate {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sumatoria de números entre 1 y 5: " + FuncionesMatematicas.sumatorio(5));
+		
+		System.out.println("Potencia de 2.5 elevado a 3: " + FuncionesMatematicas.potencia(2.5, 3));
+
+		System.out.println("La posición 7 corresponde en la secuencia de fibonacci a: " + FuncionesMatematicas.serieFibonacci(8));
+	}
+
+}
