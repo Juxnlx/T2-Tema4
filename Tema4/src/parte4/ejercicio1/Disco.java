e1356dd97e41ccd0721b341f8160b425758cd18a
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-02-20 20:57:30 +0100

    Ejercicio 4 CRUD - En proceso...

---
+++ Tema4/src/parte4/ejercicio1/Disco.java
@@ -0,0 +1,20 @@
+package parte4.ejercicio1;
+
+public class Disco {
+	
+	//Creamos el atributo codigo como int para almacenar el codigo del disco.
+	private int codigo;
+	
+	//Creamos el atributo autor como String para almacenar el autor del disco.
+	private String disco;
+	
+	//Creamos el atributo titulo como String para almacenar el titulo del disco.
+	private String titulo;
+	
+	//Creamos el atributo duración como int para almacenar la duración del disco.
+	private int duracion;
+	
+	/*PREGUNTAR A ELENA SI ESTO ES UN ENUM*/
+	//Creamos el atributo genero como String para almacenar el genero del disco.
+	private String genero;
+}
